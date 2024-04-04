a08d263a1bcc395c75a7be6b606f1fa07c3603f5
simoon <420526391@qq.com>
2024-04-05 07:18:05 +0800

    完成全局的exception

--- src/main/java/com/tlpy8/shop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/tlpy8/shop/exception/GlobalExceptionHandler.java
@@ -1,13 +1,26 @@
 package com.tlpy8.shop.exception;
 
 import com.tlpy8.shop.common.ApiRestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(Exception.class)
+    @ResponseBody
     public Object handleException (Exception e) {
-        return ApiRestResponse.error(ImoocMallExceptionEnum.SYSTEM_ERROR)
+        log.error("DefaultException", e);
+        return ApiRestResponse.error(ImoocMallExceptionEnum.SYSTEM_ERROR);
+    }
+
+    @ExceptionHandler(ImoocMallException.class)
+    @ResponseBody
+    public Object handleImoocMallException (ImoocMallException e) {
+        log.error("ImoocMallException", e);
+        return ApiRestResponse.error(e.getCode(), e.getMessage());
     }
 }
